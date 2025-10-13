39284d95a614bde97098c41efac62a4bd68c7bea
Ben Edwards <hello@benedfit.com>
2025-10-13 16:38:56 +0100

    chore: cleanup

--- src/utils/format.js
+++ src/utils/format.js
@@ -2,22 +2,6 @@ import config from '~config'
 
 const { name } = config
 
-const getOrdinal = number =>
-  number > 0
-    ? ['th', 'st', 'nd', 'rd'][
-        (number > 3 && number < 21) || number % 10 > 3 ? 0 : number % 10
-      ]
-    : undefined
-
-export const formatDate = date => {
-  const day = date.getDate()
-  const ordinal = getOrdinal(day)
-  const month = date.toLocaleString('en', { month: 'long' })
-  const year = date.getFullYear()
-
-  return `${month} ${day}${ordinal}, ${year}`
-}
-
 export const mailto = email => `mailto:${email}`
 
 export const slugify = text =>
