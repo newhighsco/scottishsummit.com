d1160a0b046176cb0d4e2e1207ba611003f0d503
Ben Edwards <hello@benedfit.com>
2025-10-16 18:56:52 +0100

    fix(video): improve lazy loading (#80)

--- src/components/Video/index.jsx
+++ src/components/Video/index.jsx
@@ -1,17 +1,32 @@
 import { ResponsiveMedia } from '@newhighsco/chipset'
+import { getImageProps } from 'next/image'
 import { string } from 'prop-types'
 import React from 'react'
 
 import styles from './Video.module.scss'
 
-export const Video = ({ id, title = '' }) => {
+export const Video = ({ id, title }) => {
   if (!id) return null
+
+  const { style, ...rest } = getImageProps({
+    src: `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`,
+    alt: title,
+    fill: true
+  }).props
+
+  const posterHtml = Object.entries(rest)
+    .reduce((html, [key, value]) => (html += ` ${key}='${value}'`), '')
+    .trim()
+
+  const src = `https://www.youtube.com/embed/${id}?autoplay=1&color=white&rel=0`
+  const srcDoc = `<style>*{padding:0;margin:0;overflow:hidden}img{position:absolute;inset:0;margin:auto;width:100%;height:auto}a::before{content:'';inset:calc(50% - 30px);position:absolute;border:30px solid transparent;border-left:60px solid white;z-index:1}</style><a href='${src}'><img ${posterHtml}/></a>`
 
   return (
     <ResponsiveMedia ratio="16:9" className={styles.root}>
       <iframe
         title={title}
-        src={`https://www.youtube.com/embed/${id}?color=white&controls=0&rel=0`}
+        src={src}
+        srcDoc={srcDoc}
         loading="lazy"
         allowFullScreen
       />
