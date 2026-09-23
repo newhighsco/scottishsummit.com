8d1ec14d856a6a70a2f195066e7c970c6fc3d5a6
Matt Beard <matt@mbeard.co.uk>
2026-09-23 15:58:13 +0100

    Revise accessibility and data sharing details (#671)
    * Revise accessibility and data sharing details
    
    Updated accessibility information and data sharing policies for event attendees. Clarified the availability of gender-neutral bathrooms and sponsorship data sharing practices.
    
    * Update policies.jsx
    
    * docs: update policies & pages
    
    * chore: fix typo
    
    * chore: remove one button from agenda
    
    * chore: remove button group header
    
    ---------
    
    Co-authored-by: Matt Beard <MattB@data-8.co.uk>

--- src/pages/agenda.jsx
+++ src/pages/agenda.jsx
@@ -35,12 +35,15 @@ const AgendaPage = () => (
       <Prose>
         <h1>{meta.title}</h1>
         <p>
-          This is your full day agenda — event app coming soon to schedule your
-          day.
+          This is your full day agenda. This is also available in the event app.
         </p>
       </Prose>
-      <Button href="https://fienta.com/scottish-summit-2026" variant="inverted">
-        Get Event Tickets
+      <Button
+        href="https://app.scottishsummit.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Access the App
       </Button>
     </Section>
     <AgendaSection
